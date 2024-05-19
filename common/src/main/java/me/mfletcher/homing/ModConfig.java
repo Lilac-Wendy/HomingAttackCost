c784598e1d2ffec9becf434fcb768b7ce71dfd24
mfletcher2 <92177225+mfletcher2@users.noreply.github.com>
2024-05-18 18:24:44 -0700

    Add server/client text to config screen

--- common/src/main/java/me/mfletcher/homing/ModConfig.java
+++ common/src/main/java/me/mfletcher/homing/ModConfig.java
@@ -4,8 +4,9 @@ import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.Config;
 import me.shedaniel.autoconfig.annotation.ConfigEntry;
 
-@Config(name = "assets.homing")
+@Config(name = "homing")
 public class ModConfig implements ConfigData {
+    @ConfigEntry.Gui.PrefixText
     public int homingRange = 20;
 
     int homingSpeed = 3;
@@ -20,6 +21,7 @@ public class ModConfig implements ConfigData {
     float defenseHomingDamageMultiplier = 0.3f;
     float toughnessHomingDamageMultiplier = 2.5f;
 
+    @ConfigEntry.Gui.PrefixText
     @ConfigEntry.BoundedDiscrete(min = 0, max = 100)
     public int reticleVolume = 100;
 }
